7e1bf7d3dc92f8502c5694b7748e204884343508
left thomas <leftthomas@qq.com>
2017-10-08 15:02:43 +0800

    重构代码

---
+++ model.py
@@ -0,0 +1,72 @@
+from keras.layers import Dense
+from keras.layers import Reshape
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.convolutional import UpSampling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
+
+
+def generator_model():
+    # 下面搭建生成器的架构，首先导入序贯模型（sequential），即多个网络层的线性堆叠
+    model = Sequential()
+    # 添加一个全连接层，输入为100维向量，输出为1024维
+    model.add(Dense(input_dim=100, output_dim=1024))
+    # 添加一个激活函数tanh
+    model.add(Activation('tanh'))
+    # 添加一个全连接层，输出为128×7×7维度
+    model.add(Dense(128 * 7 * 7))
+    # 添加一个批量归一化层，该层在每个batch上将前一层的激活值重新规范化，即使得其输出数据的均值接近0，其标准差接近1
+    model.add(BatchNormalization())
+    model.add(Activation('tanh'))
+    # Reshape层用来将输入shape转换为特定的shape，将含有128*7*7个元素的向量转化为7×7×128张量
+    model.add(Reshape((7, 7, 128), input_shape=(128 * 7 * 7,)))
+    # 2维上采样层，即将数据的行和列分别重复2次
+    model.add(UpSampling2D(size=(2, 2)))
+    # 添加一个2维卷积层，卷积核大小为5×5，激活函数为tanh，共64个卷积核，并采用padding以保持图像尺寸不变
+    model.add(Conv2D(64, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    model.add(UpSampling2D(size=(2, 2)))
+    # 卷积核设为1即输出图像的维度
+    model.add(Conv2D(1, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    return model
+
+
+def discriminator_model():
+    # 下面搭建判别器架构，同样采用序贯模型
+    model = Sequential()
+
+    # 添加2维卷积层，卷积核大小为5×5，激活函数为tanh，输入shape在‘channels_first’模式下为（samples,channels，rows，cols）
+    # 在‘channels_last’模式下为（samples,rows,cols,channels），输出为64维
+    model.add(
+        Conv2D(64, (5, 5),
+               padding='same',
+               input_shape=(28, 28, 1))
+    )
+    model.add(Activation('tanh'))
+    # 为空域信号施加最大值池化，pool_size取（2，2）代表使图片在两个维度上均变为原长的一半
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (5, 5)))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # Flatten层把多维输入一维化，常用在从卷积层到全连接层的过渡
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('tanh'))
+    # 一个结点进行二值分类，并采用sigmoid函数的输出作为概念
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    return model
+
+
+def generator_containing_discriminator(g, d):
+    # 将前面定义的生成器架构和判别器架构组拼接成一个大的神经网络，用于判别生成的图片
+    model = Sequential()
+    # 先添加生成器架构，再令d不可训练，即固定d
+    # 因此在给定d的情况下训练生成器，即通过将生成的结果投入到判别器进行辨别而优化生成器
+    model.add(g)
+    d.trainable = False
+    model.add(d)
+    return model
--- train_test.py
+++ train_test.py
@@ -1,81 +1,11 @@
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Reshape
-from keras.layers.core import Activation
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import UpSampling2D
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers.core import Flatten
-from keras.optimizers import SGD
-from keras.datasets import mnist
-import numpy as np
-from PIL import Image
-import argparse
 import math
 
+import numpy as np
+from PIL import Image
+from keras.datasets import mnist
+from keras.optimizers import SGD
 
-def generator_model():
-    #下面搭建生成器的架构，首先导入序贯模型（sequential），即多个网络层的线性堆叠
-    model = Sequential()
-    #添加一个全连接层，输入为100维向量，输出为1024维
-    model.add(Dense(input_dim=100, output_dim=1024))
-    #添加一个激活函数tanh
-    model.add(Activation('tanh'))
-    #添加一个全连接层，输出为128×7×7维度
-    model.add(Dense(128*7*7))
-    #添加一个批量归一化层，该层在每个batch上将前一层的激活值重新规范化，即使得其输出数据的均值接近0，其标准差接近1
-    model.add(BatchNormalization())
-    model.add(Activation('tanh'))
-    #Reshape层用来将输入shape转换为特定的shape，将含有128*7*7个元素的向量转化为7×7×128张量
-    model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))
-    #2维上采样层，即将数据的行和列分别重复2次
-    model.add(UpSampling2D(size=(2, 2)))
-    #添加一个2维卷积层，卷积核大小为5×5，激活函数为tanh，共64个卷积核，并采用padding以保持图像尺寸不变
-    model.add(Conv2D(64, (5, 5), padding='same'))
-    model.add(Activation('tanh'))
-    model.add(UpSampling2D(size=(2, 2)))
-    #卷积核设为1即输出图像的维度
-    model.add(Conv2D(1, (5, 5), padding='same'))
-    model.add(Activation('tanh'))
-    return model
-
-
-def discriminator_model():
-    # 下面搭建判别器架构，同样采用序贯模型
-    model = Sequential()
-
-    # 添加2维卷积层，卷积核大小为5×5，激活函数为tanh，输入shape在‘channels_first’模式下为（samples,channels，rows，cols）
-    # 在‘channels_last’模式下为（samples,rows,cols,channels），输出为64维
-    model.add(
-        Conv2D(64, (5, 5),
-               padding='same',
-               input_shape=(28, 28, 1))
-    )
-    model.add(Activation('tanh'))
-    # 为空域信号施加最大值池化，pool_size取（2，2）代表使图片在两个维度上均变为原长的一半
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, (5, 5)))
-    model.add(Activation('tanh'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    # Flatten层把多维输入一维化，常用在从卷积层到全连接层的过渡
-    model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Activation('tanh'))
-    # 一个结点进行二值分类，并采用sigmoid函数的输出作为概念
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
-    return model
-
-
-def generator_containing_discriminator(g, d):
-    #将前面定义的生成器架构和判别器架构组拼接成一个大的神经网络，用于判别生成的图片
-    model = Sequential()
-    #先添加生成器架构，再令d不可训练，即固定d
-    #因此在给定d的情况下训练生成器，即通过将生成的结果投入到判别器进行辨别而优化生成器
-    model.add(g)
-    d.trainable = False
-    model.add(d)
-    return model
+from model import generator_model, discriminator_model, generator_containing_discriminator
 
 
 def combine_images(generated_images):
@@ -95,8 +25,6 @@ def combine_images(generated_images):
 
 
 def train(BATCH_SIZE):
-    # 国内好像不能直接导入数据集，我们试了几次都不行，后来将数据集下载到本地'~/.keras/datasets/'，也就是当前目录（我的是用户文件夹下）下的.keras文件夹中。
-    # 下载的地址为：https://s3.amazonaws.com/img-datasets/mnist.npz
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
     # iamge_data_format选择"channels_last"或"channels_first"，该选项指定了Keras将要使用的维度顺序。
     # "channels_first"假定2D数据的维度顺序为(channels, rows, cols)，3D数据的维度顺序为(channels, conv_dim1, conv_dim2, conv_dim3)
@@ -175,19 +103,19 @@ def train(BATCH_SIZE):
 
             # 每100次迭代保存一次生成器和判别器的权重
             if index % 100 == 9:
-                g.save_weights('generator', True)
-                d.save_weights('discriminator', True)
+                g.save_weights('generator.weights', True)
+                d.save_weights('discriminator.weights', True)
 
 
-def generate(BATCH_SIZE, nice= False ):
+def test(BATCH_SIZE, nice=False):
     #训练完模型后，可以运行该函数生成图片
     g = generator_model()
     g.compile(loss='binary_crossentropy', optimizer="SGD")
-    g.load_weights('generator')
+    g.load_weights('generator.weights')
     if nice:
         d = discriminator_model()
         d.compile(loss='binary_crossentropy', optimizer="SGD")
-        d.load_weights('discriminator')
+        d.load_weights('discriminator.weights')
         noise = np.random.uniform(-1, 1, (BATCH_SIZE*20, 100))
         generated_images = g.predict(noise, verbose=1)
         d_pret = d.predict(generated_images, verbose=1)
@@ -211,4 +139,4 @@ def generate(BATCH_SIZE, nice= False ):
 
 
 # train(132)
-generate(132)
+test(132)
